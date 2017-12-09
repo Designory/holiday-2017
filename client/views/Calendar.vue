9292b149a4e7d05ece7b0d5770a3d4150ead709d
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-09 01:18:00 -0600

    update

--- client/views/Calendar.vue
+++ client/views/Calendar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="calendar">
-    <select class="calendar__elm calendar__elm--month" name="month">
-      <option value="volvo">Jan</option>
-      <option value="saab">Feb</option>
-      <option value="fiat">Mar</option>
-      <option value="audi">Apr</option>
+    <select class="calendar__field calendar__field--month" name="month">
+      <option class="calendar__item" value="volvo">Jan</option>
+      <option class="calendar__item" value="saab">Feb</option>
+      <option class="calendar__item" value="fiat">Mar</option>
+      <option class="calendar__item" value="audi">Apr</option>
     </select>
-    <select class="calendar__elm calendar__elm--date" name="date">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
+    <select class="calendar__field calendar__field--date" name="date">
+      <option class="calendar__item" value="1">1</option>
+      <option class="calendar__item" value="2">2</option>
+      <option class="calendar__item" value="3">3</option>
+      <option class="calendar__item" value="4">4</option>
     </select>
   </div>
 </template>
