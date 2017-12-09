49019636b6e704825c70e3214dd529afc979f062
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-09 00:36:37 -0600

    custom

--- client/views/Calendar.vue
+++ client/views/Calendar.vue
@@ -1,3 +1,23 @@
 <template>
-  <p class="txt">Calendar</p>
+  <div class="calendar">
+    <div class="calendar__field">
+      <div class="calandar__month" data-month="1">Jan</div>
+      <div class="calandar__month" data-month="2">Feb</div>
+      <div class="calandar__month" data-month="3">Mar</div>
+      <div class="calandar__month" data-month="4">Apr</div>
+    </div>
+    <div class="calendar__field">
+      <div class="calendar__date" data-date="1">1</div>
+      <div class="calendar__date" data-date="2">2</div>
+      <div class="calendar__date" data-date="3">3</div>
+      <div class="calendar__date" data-date="3">4</div>
+      <div class="calendar__date" data-date="3">5</div>
+      <div class="calendar__date" data-date="3">6</div>
+      <div class="calendar__date" data-date="3">7</div>
+      <div class="calendar__date" data-date="3">8</div>
+      <div class="calendar__date" data-date="3">9</div>
+      <div class="calendar__date" data-date="3">10</div>
+      <div class="calendar__date" data-date="3">11</div>
+    </div>
+  </div>
 </template>
