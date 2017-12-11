12d92ce6d4487d8a832cb0ab6209d5bcbcccc08c
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-11 11:30:14 -0600

    date view upd

--- client/views/Date.vue
+++ client/views/Date.vue
@@ -1,3 +1,22 @@
 <template>
-  <p class="txt">Date</p>
+  <div>
+    <p class="txt txt--center">Month: {{month}}</p>
+    <p class="txt txt--center">Date: {{date}}</p>
+    <p class="txt txt--center">Description</p>
+    
+  </div>
 </template>
+
+<script>
+
+export default {
+  computed: {
+    month() {
+      return this.$store.state.params.month;
+    },
+    date() {
+      return this.$store.state.params.date;
+    }
+  }
+}
+</script>
