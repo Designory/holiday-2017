28723869057b071e1516ed967d073f10aef71453
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-11 22:32:50 -0600

    socials extracted

--- client/views/Date.vue
+++ client/views/Date.vue
@@ -3,52 +3,20 @@
     <p class="txt">On {{month}}/{{date}}, I'll be celebrating</p>
     <p class="txt txt--center">{{title}}</p>
     <p class="txt txt--center">{{description}}</p>
+
     <div @click="randomize()">RANDOMIZE</div>
     
-    <social-sharing url="http://localhost:4000/"
-                      title="Holiday Card"
-                      description="Lorem ipsum"
-                      quote="abc"
-                      hashtags="designory, holidaycard"
-                      inline-template>
-
-      <div class="social">
-        <network network="email" class="social__elm">
-            <i class="fa fa-envelope"></i> Email
-        </network>
-        <network network="facebook" class="social__elm">
-          <i class="fa fa-facebook"></i> Facebook
-        </network>
-        <network network="googleplus" class="social__elm">
-          <i class="fa fa-google-plus"></i> Google +
-        </network>
-        <network network="linkedin" class="social__elm">
-          <i class="fa fa-linkedin"></i> LinkedIn
-        </network>
-        <network network="pinterest" class="social__elm">
-          <i class="fa fa-pinterest"></i> Pinterest
-        </network>
-        <network network="sms" class="social__elm">
-          <i class="fa fa-commenting-o"></i> SMS
-        </network>
-        <network network="twitter" class="social__elm">
-          <i class="fa fa-twitter"></i> Twitter
-        </network>
-      </div>
-    </social-sharing> 
+    <Social></Social>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import SocialSharing from 'vue-social-sharing';
+import Social from '../components/Social';
 import { setDate } from '../utils';
-
-Vue.use(SocialSharing);
 
 export default {
   components: {
-    SocialSharing
+    Social
   },
   computed: {
     month() {
@@ -58,7 +26,6 @@ export default {
       return this.$store.state.params.date;
     },
     title() {
-      // store.state.holiday = findHoliday(this.$store.state.params.month, this.$store.state.params.date)
       return this.$store.state.holiday.title;
     },
     description() {   
