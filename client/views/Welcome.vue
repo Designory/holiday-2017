b45ece6b1ff17e617f675f910047fc99503bd312
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-13 13:57:11 -0600

    done

--- client/views/Welcome.vue
+++ client/views/Welcome.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="welcome">
-    <p class="txt">
+    <p class="txt txt--thin-lg txt--center-lg">
        While we do want to wish you a very happy holiday season, we here at Designory want the world to know that every day is a good day to celebrate. 
     </p>
-    <p class="txt txt--strong">
+    <p class="txt txt--strong txt--center-lg">
        Enter a day — any day. And we’ll show you your reason to celebrate.
     </p>
   </div>
