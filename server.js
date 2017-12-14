ed10dc0e8421badfeb69011f83a8ce23e274195a
kevin-middlesworth-designory-com <kevin.middlesworth@designory.com>
2017-12-14 10:38:42 -0600

    adding static folder to server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('dist'))
+app.use(express.static('static'))
 //app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${process.env.PORT || 3000}`))
