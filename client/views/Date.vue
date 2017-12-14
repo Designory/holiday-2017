1e2acb803d1f9dc66c8ad7678b537f1c3109c466
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-13 19:27:21 -0600

    almost

--- client/views/Date.vue
+++ client/views/Date.vue
@@ -60,9 +60,9 @@ export default {
       return this.$store.state.holiday.description;
     },
     height() {
-      // you mioght think why? ¯\_(ツ)_/¯
+      // you might think why? ¯\_(ツ)_/¯
       // well, because of damned ios safari dynamic nav bars
-      return this.$store.state.screen.height - (this.$store.state.screen.width < 1025 ? 190 : 290);
+      return this.$store.state.screen.height - (this.$store.state.screen.width < 1025 ? 190 : 320);
     }
   },
   methods: {
