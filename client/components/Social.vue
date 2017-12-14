071a6ecf84fda1055863dccdbfd5f6f792b5d056
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-13 20:09:11 -0600

    done

--- client/components/Social.vue
+++ client/components/Social.vue
@@ -7,22 +7,19 @@
                       inline-template>
                       
         <div class="social">
-            <network network="email" class="social__elm">
+            <network network="email" class="social__elm social__elm--email">
                  Email
             </network>
-            <network network="facebook" class="social__elm">
+            <network network="facebook" class="social__elm social__elm--fb">
                  Facebook
             </network>
-            <network network="linkedin" class="social__elm">
+            <network network="linkedin" class="social__elm social__elm--linked">
                  LinkedIn
             </network>
-            <!-- <network network="pinterest" class="social__elm">
-                 Pinterest
-            </network> -->
-            <network network="sms" class="social__elm">
+            <network network="sms" class="social__elm social__elm--sms">
                 SMS
             </network>
-            <network network="twitter" class="social__elm">
+            <network network="twitter" class="social__elm social__elm--twitter">
                  Twitter
             </network>
         </div>
