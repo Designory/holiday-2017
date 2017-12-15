7c8804fed1251ede1b1252799559d00c7e26d815
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-14 21:40:15 -0600

    update

--- build/config.js
+++ build/config.js
@@ -6,8 +6,8 @@ module.exports = {
   // Options for webpack-dev-server
   // See https://webpack.js.org/configuration/dev-server
   devServer: {
-    host: '10.89.236.189',
-    // host: 'localhost',
+    // host: '10.89.236.189',
+    host: 'localhost',
     port: 4000
   },
   // when you use electron please set to relative path like ./
