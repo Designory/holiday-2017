e1b8f3ba970946620a1fba20733bb48ffa0b481e
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-12-17 13:43:33 -0600

    tmp save

--- client/views/Welcome.vue
+++ client/views/Welcome.vue
@@ -1,35 +1,38 @@
 <template>
 <div>
+    <!-- iPhone 6 (375px) and higher text -->
     <div class="welcome">
         <p class="txt txt--thin txt--center-lg">
           Why only celebrate <br>
-    A couple times a year?<br>
-    We at Designory believe<br>
-    In continuously spreading cheer!
+          A couple times a year?<br>
+          We at Designory believe<br>
+          In continuously spreading cheer!
         </p>
         <p class="txt txt--strong txt--center-lg">
           Push those doubts away, <br>
-    We’ve dug deep, and found <br>
-    A gloriously real holiday <br>
-    For each day, year round!
+          We’ve dug deep, and found <br>
+          A gloriously real holiday <br>
+          For each day, year round!
           <span class="navigation__btn navigation__btn--next navigation__btn--desktop" @click="$store.dispatch('nextView')"></span>
         </p>
     </div>
 
+    <!-- Below 375px => aka iPhone 5 -->
     <div class="welcome welcome--ip5">
 
       <p class="txt txt--thin txt--center-lg">
         Why only celebrate 
-  a couple times a year?
-  We at Designory believe
-  In continuously spreading cheer!
+        a couple times a year?
+        We at Designory believe
+        In continuously spreading cheer!
       </p>
       <p class="txt txt--strong txt--center-lg">
         Push those doubts away, 
-  we’ve dug deep, and found 
-  a gloriously real holiday 
-  for each day, year round!
+        we’ve dug deep, and found 
+        a gloriously real holiday 
+        for each day, year round!
       </p>
   </div>
+  
 </div>
 </template>
