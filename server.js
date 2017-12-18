2beac8420d8e60e9b07bf5f02e72a209357c5e79
kevin-middlesworth-designory-com <kevin.middlesworth@designory.com>
2017-12-18 11:15:35 -0600

    robots

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-// var express = require('express');
-// var server = express();
-// server.use('/', express.static(__dirname + '/dist/'));
-// server.listen(process.env.PORT || 3000);
-
-console.log(process.env);
-
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const app = express();
 const compression = require('compression');
 const basicAuth = require('express-basic-auth');
 const cors = require('cors');
 const sslRedirect = require('heroku-ssl-redirect');
-
+const robots = require('express-robots');
 
 // redirect if http, to https
 app.use(sslRedirect());
@@ -25,9 +17,14 @@ app.use(compression({
 	filter: function () { return true; }
 }));
 
+// disable crawling anything but production
+if (process.env.NODE_ENV !== 'production') {
+	app.use(robots({UserAgent: '*', Disallow: '/'}));
+}
+
+
 app.use(express.static('dist'));
 app.use('/static', express.static('static'));
-
 
 // authenticate
 if (process.env.PASSWORD && process.env.USERNAME) {
